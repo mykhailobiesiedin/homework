1642a11dc3d4cbb8ba88446c86a7f1f0190ae911
mykhailobiesiedin <122889486+mykhailobiesiedin@users.noreply.github.com>
2023-02-01 12:47:05 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,36 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        int longWallWidth = 15;
-        int longWallHeight = 10;
-        int shortWallWidth = 12;
-        int shortWallHeight = 9;
-        int doorHeight = 5;
-        int doorWidth = 2;
-        int windowHeight = 4;
-        int windowWidth = 3;
-        int wallpaperHeight = 13;
-        int wallpaperWidth = 2;
+        Scanner keyboard = new Scanner(System.in);
 
-        int longWallArea = longWallWidth * longWallHeight;
-        int shortWallArea = shortWallWidth * shortWallHeight;
-        int doorArea = doorHeight * doorWidth;
-        int windowArea = windowHeight * windowWidth;
-        int windowDoorArea = doorArea + windowArea;
-        int roomArea = (longWallArea + shortWallArea) *2;
-        int withoutStuff = roomArea - windowDoorArea;
-        int wallpaperArea = wallpaperHeight * wallpaperWidth;
-        int wallpaperAmount = withoutStuff / wallpaperArea;
+        System.out.println("Choose a number :");
+        int arg1 = keyboard.nextInt(2);
 
-        System.out.println(longWallWidth + "*" + longWallHeight + "=" + longWallArea);
-        System.out.println(shortWallWidth + "*" + shortWallHeight + "=" + shortWallArea);
-        System.out.println((longWallArea + "+" + shortWallArea) + "*2" + "=" +roomArea);
-        System.out.println(doorHeight + "*" + doorWidth + "=" + doorArea);
-        System.out.println(windowHeight + "*" + windowWidth + "=" + windowArea);
-        System.out.println(doorArea +"+" + windowArea + "=" + windowDoorArea);
-        System.out.println( roomArea + "-" + windowDoorArea + "=" + withoutStuff);
-        System.out.println(wallpaperHeight + "*" + wallpaperWidth + "=" + wallpaperArea);
-        System.out.println(withoutStuff + "/" + wallpaperArea + "=" + wallpaperAmount);
+        System.out.println("Choose a second number :");
+        int arg2 = keyboard.nextInt(2);
+
+        int res = arg1 + arg2;
+
+        System.out.println("Your result :" + Integer.toBinaryString(arg1) + "+" +
+                Integer.toBinaryString(arg2) + "=" + res);
+        System.out.println("Your result :" + Integer.toBinaryString(arg1) + "+" +
+                Integer.toBinaryString(arg2) + "=" + Integer.toBinaryString(res));
+
+
+
 
 
     }
